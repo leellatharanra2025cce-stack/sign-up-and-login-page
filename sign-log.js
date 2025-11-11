8bad45dffb15c0b06195c9ede444d902663e91af
leellatharanra2025cce-stack <leellatharan.ra2025cce@sece.ac.in>
2025-11-11 16:01:44 +0530

    Add files via upload

---
+++ sign-log.js
@@ -0,0 +1,11 @@
+function showsignup(){
+    document.getElementById("login").style.display='none';
+    document.getElementById("signup").style.display='block';
+}
+function showLogin(){
+    
+    document.getElementById("signup").style.display='none';
+    document.getElementById("login").style.display='block';
+}
+
+    
